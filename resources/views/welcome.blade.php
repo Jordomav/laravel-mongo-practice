169c7c67183109b100fb6ba6256fd10c9e84e004
Jordomav <jordomav@gmail.com>
2016-03-31 13:18:32 -0700

    bower created

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,15 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <title>Laravel/Angular/Mongo</title>
+        <link rel="stylesheet" href="build/css/css-deps.css">
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <h1 class="title">Angular, Laravel and Mongo</h1>
             </div>
         </div>
     </body>
+    <script src="build/js/js-deps.js"></script>
 </html>
