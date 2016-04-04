c165e264c37e97c0328188c89fd5dc6962dd5308
supaheckafresh <neilstrain@gmail.com>
2016-04-04 09:44:44 -0700

    Add project migration files

---
+++ database/migrations/2016_04_01_204439_create_questions_collection.php
@@ -0,0 +1,34 @@
+<?php
+
+use Jenssegers\Mongodb\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsCollection extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $collection) {
+            $collection->increments('id');
+            $collection->timestamps();
+            $collection->string('text');
+            $collection->string('help_url');
+            $collection->string('data_type');
+            $collection->boolean('default_question');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions');
+    }
+}
