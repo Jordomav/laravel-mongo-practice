45221b00bac0330939eab2795ec20af224b7b812
denajunnila <dena.junnila@sbcglobal.net>
2016-04-04 09:56:40 -0700

    oops

---
+++ app/Answer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class Answer extends Eloquent
+{
+        public function questions()
+    {
+    	return $this->belongsTo(Question::class);
+    }
+}
