71631c20c0e5b4c47dc06bac1d428b96795829bf
supaheckafresh <neilstrain@gmail.com>
2016-04-04 09:47:52 -0700

    Add Question model

---
+++ app/Question.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class Question extends Eloquent
+{
+    public function answers()
+    {
+        return $this->embedsMany(Answer::class);
+    }
+}
