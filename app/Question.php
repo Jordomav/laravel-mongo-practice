0fb9e9de67347f1f2c19b6f1474d0b209b0c693a
supaheckafresh <neilstrain@gmail.com>
2016-04-04 10:33:42 -0700

    Add fillable fields to Question model

--- app/Question.php
+++ app/Question.php
@@ -6,6 +6,8 @@ use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
 class Question extends Eloquent
 {
+    protected $fillable = ['text', 'help_url', 'data_type', 'default_question'];
+
     public function answers()
     {
         return $this->embedsMany(Answer::class);
