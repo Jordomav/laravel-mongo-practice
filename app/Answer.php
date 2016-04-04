a1a6ece9fa64cc59cd8c16fdb1be499c2c559b03
supaheckafresh <neilstrain@gmail.com>
2016-04-04 13:09:56 -0700

    Add fillable fields for Answer

--- app/Answer.php
+++ app/Answer.php
@@ -6,6 +6,8 @@ use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
 class Answer extends Eloquent
 {
+    protected $fillable = ['text', 'compliant'];
+
     public function question()
     {
         return $this->belongsTo(Question::class);
