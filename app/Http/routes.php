7d405b60c1a3b618ad920c1eee98f2769a318ccd
vagrant <vagrant@homestead>
2016-04-05 17:57:49 +0000

    Update get '/' and 'get-questions' routes in routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,8 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('questionnaire');
 });
 
-
-Route::get('show', 'DatabaseController@show');
+Route::get('get-questions', 'QuestionsController@index');
