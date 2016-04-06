cac7f009cb7d5c92596a01faf5db50a58d26fd11
Jordomav <jordomav@gmail.com>
2016-04-06 13:04:56 -0700

    Comments added

--- resources/views/questionnaire.blade.php
+++ resources/views/questionnaire.blade.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet" href="/build/css/css-deps.css">
         <link rel="stylesheet" href="/build/css/app.css">
     </head>
+    {{--Angular Controller and App Connected--}}
     <body data-ng-app="adaApp" data-ng-controller="QuestionnaireController as questionnaire">
     <div class="container">
         <div class="row">
@@ -29,16 +30,26 @@
                     <form data-ng-if="question.data_type === 'number'" class="col-xs-2">
                         <input type="number" class="form-control">
                     </form>
+
+                     {{--Save Status Icons--}}
+                    {{--<div class="notifyBox">--}}
+                        {{--<i class="fa fa-times-circle fa-2x notify"></i>--}}
+                    {{--</div>--}}
+                    {{--<i class="fa fa-check fa-2x notify"></i>--}}
+
                 </div>
                 <br>
+
+                {{--Compliance Pop-outs--}}
                 {{--Compliant Pop-out--}}
-                <div class="compliance">
-                    <div class="compliant"><h4><i class="fa fa-check icon-size"></i> You are ADA compliant.</h4></div>
-                </div>
+                {{--<div class="compliance">--}}
+                    {{--<div class="compliant"><h4><i class="fa fa-check icon-size"></i> You are ADA compliant.</h4></div>--}}
+                {{--</div>--}}
                 {{--Non-Compliant Pop-Out--}}
-                <div class="compliance">
-                    <div class="non-compliant"><h4><i class="fa fa-times-circle icon-size"></i> You are NOT ADA compliant.</h4></div>
-                </div>
+                {{--<div class="compliance">--}}
+                    {{--<div class="non-compliant"><h4><i class="fa fa-times-circle icon-size"></i> You are NOT ADA compliant.</h4></div>--}}
+                {{--</div>--}}
+
             </div>
         </div>
     </div>
