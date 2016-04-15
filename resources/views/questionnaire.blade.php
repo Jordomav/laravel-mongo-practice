39ae9fbd2e89cc6120cedaa3e6f686e939ec1c32
Neil Strain <neilstrain@gmail.com>
2016-04-15 10:24:37 -0700

    feat(Restyle): restructure question markup to make more sense and restructure bootstrap column widths

--- resources/views/questionnaire.blade.php
+++ resources/views/questionnaire.blade.php
@@ -11,55 +11,34 @@
     <body data-ng-app="adaApp" data-ng-controller="QuestionnaireController as questionnaire">
     <div class="container">
 
-        <h1>ADA Compliance Survey</h1>
-        <div data-ng-repeat="question in questionnaire.questions">
+        <h1 class="row">ADA Compliance Survey</h1>
 
-            <div class="row">
-                <div class="col-lg-7 question">
-                    {{-- Display the question --}}
-                    <p>@{{ question.text }}</p>
+        <div data-ng-repeat="question in questionnaire.questions" class="question row">
 
-                    {{-- Display appropriate input type to allow user to answer question --}}
-                    <multiple-choice-input data-ng-if="question.data_type === 'multiple_choice'"></multiple-choice-input>
-                    <true-false-input data-ng-if="question.data_type === 'true_false'"></true-false-input>
-                    <range-input data-ng-if="question.data_type === 'range'"></range-input>
-                </div>
+            <div class="col-xs-8">
+                {{-- Display the question --}}
+                <p>@{{ question.text }}</p>
 
-                {{-- Compliance Pop-out for each question --}}
-                <div data-ng-show="questionnaire.getWasAnswered(question)"
-                     data-ng-class="{someClass: hover}"
-                     data-ng-mouseenter="hover = true"
-                     data-ng-mouseleave="hover = false"
-                     class="compliance col-lg-4">
+                {{-- Display appropriate input type to allow user to answer question --}}
+                <multiple-choice-input data-ng-if="question.data_type === 'multiple_choice'"></multiple-choice-input>
+                <true-false-input data-ng-if="question.data_type === 'true_false'"></true-false-input>
+                <range-input data-ng-if="question.data_type === 'range'"></range-input>
 
-                    {{-- Display when question answer is compliant --}}
-                    <div data-ng-show="question.compliant === true"
-                         class="compliant">
-                        <h4><i class="fa fa-check icon-size"></i> You are ADA compliant.</h4>
-                    </div>
-
-                    {{-- Display when question answer is noncompliant --}}
-                    <div data-ng-hide="question.compliant === true"
-                         class="non-compliant">
-                        <h4><i class="fa fa-times-circle icon-size"></i> You are NOT ADA compliant.</h4>
-                    </div>
-                </div>
-
+                {{-- Save message --}}
+                <p class="save-message row text-right">saved</p>
             </div>
 
-            <div class="row notifier">
-                <div class="col-lg-7">
-                    {{--Save Status Icons--}}
-                    <div class="notifyBox" >
-                        <i data-ng-hide="questionnaire.getWasAnswered(question)"
-                           class="fa fa-times-circle fa-2x notify"></i>
-                    </div>
-                    <div class="notifyBox">
-                        <i data-ng-show="questionnaire.getWasAnswered(question)"
-                           style="color:#00AA00;"
-                           class="fa fa-check fa-2x notify"
-                           id="flip"></i>
-                    </div>
+            <div class="col-xs-4">
+                {{-- Display when question answer is compliant --}}
+                <div data-ng-show="question.compliant === true"
+                     class="compliant">
+                    <h4><i class="fa fa-check icon-size"></i> You are ADA compliant.</h4>
+                </div>
+
+                {{-- Display when question answer is noncompliant --}}
+                <div data-ng-hide="question.compliant === true"
+                     class="non-compliant">
+                    <h4><i class="fa fa-times-circle icon-size"></i> You are NOT ADA compliant.</h4>
                 </div>
             </div>
 
