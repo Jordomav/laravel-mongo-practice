5d97d726c33fe0f090d14af4bc151cb7ad3d5147
supaheckafresh <neilstrain@gmail.com>
2016-04-26 11:47:49 -0700

    try different way of querying db

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -18,25 +18,20 @@ class QuestionnaireController extends Controller
 
     public function index()
     {
-        // Mongolab
-        $uri = 'mongodb://heroku_nhdqq3wv:8hu1n8gguitftgnql27fte63k1@ds025459.mlab.com:25459/heroku_nhdqq3wv';
-        $client = new MongoClient($uri);
-        $db = $client->selectDB('heroku_nhdqq3wv');
-
         // This will work differently when we have users set up. For now we check if we've already copied master list of
         // questions to a questionnaire object.
-        if ($db->questionnaires->first()) {
+        if (DB::collection('questionnaires')->get()->first()) {
 
-            $questionnaire = $db->questionnaires->first();
+            $questionnaire = DB::collection('questionnaires')->get()->first();
 
         } else {
-            
+
             $questionnaire = new Questionnaire;
 
             // We actually don't really need this, but hard-code user id for now.
             $questionnaire->user_id = 1;
 
-            foreach ($db->questions->get() as $q) {
+            foreach (Question::all() as $q) {
                 $questionnaire->questions()->associate($q);
             }
 
